d9e689f4a7fd427114486025440c2acd2a7867b1
Alexei Sholik <alcosholik@gmail.com>
2012-06-10 16:17:06 +0300

    Fix typos in cards.js

--- mwalimu/cards.js
+++ mwalimu/cards.js
@@ -11,60 +11,60 @@ var Cards = {
         "en": "Things are going well."
     },
 
-    "CoffeeBreak.1.4": {
+    "CoffeeBreak.1.3": {
         "fr": "Ça ne va pas.",
         "en": "Things aren't going well."
     },
 
-    "CoffeeBreak.1.5": {
+    "CoffeeBreak.1.4": {
         "ru": "хорошо",
         "fr": "bien",
         "en": "well"
     },
 
-    "CoffeeBreak.1.6": {
+    "CoffeeBreak.1.5": {
         "ru": "спасибо",
         "fr": "merci",
         "en": "thanks"
     },
 
-    "CoffeeBreak.1.7": {
+    "CoffeeBreak.1.6": {
         "ru": "превосходно",
         "fr": "excellent",
         "en": "excellent"
     },
 
-    "CoffeeBreak.1.8": {
+    "CoffeeBreak.1.7": {
         "ru": "да",
         "fr": "oui",
         "en": "yes"
     },
 
-    "CoffeeBreak.1.9": {
+    "CoffeeBreak.1.8": {
         "ru": "нет",
         "fr": "non",
         "en": "no"
     },
 
-    "CoffeeBreak.1.10": {
+    "CoffeeBreak.1.9": {
         "ru": "очень легко",
         "fr": "très facile",
         "en": "very easy"
     },
 
-    "CoffeeBreak.1.11": {
+    "CoffeeBreak.1.10": {
         "ru": "Я в хорошей форме.",
         "fr": "Je suis en forme.",
         "en": "I'm in a good form."
     },
 
-    "CoffeeBreak.1.12": {
+    "CoffeeBreak.1.11": {
         "ru": "Я устал.",
         "fr": "Je suis fatigué.",
         "en": "I'm tired."
     },
 
-    "CoffeeBreak.1.13": {
+    "CoffeeBreak.1.12": {
         "ru": "до скорого",
         "fr": "à la prochaine",
         "en": "until next time"
