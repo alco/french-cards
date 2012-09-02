55c38dbceedb32e298a9e30ef24c50078228acf6
Alexei Sholik <alcosholik@gmail.com>
2012-09-02 21:07:25 +0300

    Word editing and layout fixes

--- word_list.js
+++ word_list.js
@@ -1,8 +1,12 @@
 $(document).ready(function() {
     var topicListElem = $('#topic-list');
     var wordListElem = $('#word-list');
-    var topics = ['LOTR', 'Dune', 'Crockery', '[no topic]'];
+    var topics = ['LOTR', 'Dune', 'Crockery', 'Hojarasca', '[no topic]'];
     var selectedTopic, selectedWord;
+    var wordEditorElem = $('#word-editor');
+    wordEditorElem.hide();
+
+    $('#words-div').height($('#topics-div').height());
 
     _.each(topics, function(obj) {
         var li_elem = $('<li></li>');
@@ -19,6 +23,7 @@ $(document).ready(function() {
             'LOTR': ['tarry', 'trough', 'founder', 'asunder', 'loom'],
             'Dune': ['rascal'],
             'Crockery': [],
+            'Hojarasca': ['ataúd', 'hombro', 'aserrín', 'esclavo', 'sacudir'],
             '[no topic]': ['wont', 'swath', 'swag']
         };
         wordListElem.children().remove();
@@ -46,6 +51,7 @@ $(document).ready(function() {
     }
 
     function selectTopic(elem) {
+        resetEditor();
         highlightTopic(elem);
         var topicName = $(elem).children('a').text();
         displayWords(topicName);
@@ -66,6 +72,57 @@ $(document).ready(function() {
     });
 
     function editWord(word) {
+        populateEditor(word);
+    }
+
+    function resetEditor() {
+        wordEditorElem.hide();
+
+        $('.translations').each(function() {
+            $(this).val('');
+        });
+        $('input[name="part-of-speech"]').each(function() {
+            this.checked = false;
+        });
+    }
+
+    function populateEditor(word) {
+        resetEditor();
+
+        var storedWords = Perseus.lookupObject('words');
+        var index = -1;
+        _.each(storedWords, function(obj, ind) {
+            if (obj['word'] == word) {
+                index = ind;
+            }
+        });
+        if (index < 0) {
+            return;
+        }
+
+        wordEditorElem.show();
+
+        var wordData = storedWords[index];
+        $('#word').val(wordData['word']);
+        $('#source-lang').val(wordData['sourceLang']);
+        _.each(wordData['partsOfSpeech'], function(obj) {
+            var correctElem;
+            $('input[name="part-of-speech"]').each(function(ind, elem) {
+                if ($(elem).parent().text() == obj) {
+                    correctElem = elem;
+                }
+            });
+            console.log(correctElem);
+            console.log(typeof(correctElem));
+            correctElem.checked = true;
+        });
+        _.each(wordData['translations'], function(obj, key) {
+            $('.translation').each(function() {
+                if ($(this).hasClass(key)) {
+                    $(this).val(obj);
+                }
+            });
+        });
     }
 
     function selectWord(elem) {
